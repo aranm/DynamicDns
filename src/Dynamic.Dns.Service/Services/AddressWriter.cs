c3fbd36e346b30e4acda6661246cd9aa59b68cad
Aran Mulholland <aranmulholland@gmail.com>
2018-12-03 12:21:55 +1100

    Add services to read and write the latest IP Address to Azure table storage

---
+++ src/Dynamic.Dns.Service/Services/AddressWriter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using Dynamic.Dns.Service.TableEntities;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Dynamic.Dns.Service.Services
+{
+    public class IpAddressWriter
+    {
+        private readonly string _storageConnectionString;
+
+        public IpAddressWriter()
+        {
+            _storageConnectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
+        }
+
+        public async Task<bool> StoreIpAddress(string ipAddress)
+        {
+            try
+            {
+                var storageAccount = CloudStorageAccount.Parse(_storageConnectionString);
+                var tableClient = storageAccount.CreateCloudTableClient();
+
+                var table = tableClient.GetTableReference("IpAddresses");
+                await table.CreateIfNotExistsAsync();
+
+                var data = new IpAddressTable(ipAddress);
+
+                // Create the TableOperation object that inserts the customer entity.
+                var insertOperation = TableOperation.Insert(data);
+
+                // Execute the insert operation.
+                await table.ExecuteAsync(insertOperation);
+                return true;
+            }
+            catch (StorageException)
+            {
+                //TODO: Write Exceptions to Error Logger
+                return false;
+            }
+            catch (Exception)
+            {
+                //TODO: Write Exceptions to Error Logger
+                return false;
+            }
+        }
+    }
+}
---
+++ src/Dynamic.Dns.Service/TableEntities/IpAddressTable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Dynamic.Dns.Service.TableEntities
+{
+    public class IpAddressTable : TableEntity
+    {
+        public IpAddressTable() : base()
+        {
+
+        }
+
+        public IpAddressTable(string ipv4Address) : base("ipaddress", (DateTime.MaxValue.Ticks - DateTime.UtcNow.Ticks).ToString("d19"))
+        {
+            Ipv4Address = ipv4Address;
+        }
+
+        public string Ipv4Address { get; set; }
+    }
+}
