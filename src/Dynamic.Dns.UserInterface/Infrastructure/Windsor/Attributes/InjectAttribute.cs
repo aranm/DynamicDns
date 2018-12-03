7985f32cdf7f6dfc33fc1cb46f92d3de2edd4caf
Aran Mulholland <aranmulholland@gmail.com>
2018-12-03 17:06:08 +1100

    Add infrastructure required for Windsor property injection in WPF

---
+++ src/Dynamic.Dns.UserInterface/Infrastructure/Windsor/Attributes/InjectAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic.Dns.UserInterface.Infrastructure.Windsor.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class InjectAttribute : Attribute { }
+}
