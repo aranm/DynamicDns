008f9f188ffe8586825a4b461533b3fa8eb901d9
Aran Mulholland <aranmulholland@gmail.com>
2018-12-03 17:08:28 +1100

    Add services installer

---
+++ src/Dynamic.Dns.UserInterface/Infrastructure/Windsor/Installers/ServicesInstaller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Dynamic.Dns.Contracts.Services;
+using Dynamic.Dns.Service.Services;
+using Dynamic.Dns.UserInterface.Infrastructure.Windsor.Injection;
+
+namespace Dynamic.Dns.UserInterface.Infrastructure.Windsor.Installers
+{
+    public class ServicesInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Kernel.ComponentModelBuilder.AddContributor(new InjectionPropertyComponentModelHelper());
+
+            container.Register(Component.For<IAddressProvider>()
+                .ImplementedBy<AddressProvider>()
+                .LifestyleTransient());
+            container.Register(Component.For<IAddressWriter>()
+                .ImplementedBy<AddressWriter>()
+                .LifestyleTransient());
+        }
+    }
+}
