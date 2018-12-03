43ffa4cb593f3107276f45f55145cde39fc26064
Aran Mulholland <aranmulholland@gmail.com>
2018-12-03 12:48:49 +1100

    Remove unused using statements

--- src/Dynamic.Dns.Web/Infrastructure/HttpRequestMessageExtensions.cs
+++ src/Dynamic.Dns.Web/Infrastructure/HttpRequestMessageExtensions.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Web;
+﻿using System.Net.Http;
 using Microsoft.Owin;
 
 namespace Dynamic.Dns.Web.Infrastructure
