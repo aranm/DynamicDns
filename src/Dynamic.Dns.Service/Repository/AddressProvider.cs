ff335e95d6fda2347643d5ce175097a728101b0f
Aran Mulholland <aranmulholland@gmail.com>
2018-12-04 11:22:22 +1100

    Update namepsaces

--- src/Dynamic.Dns.Service/Repository/AddressProvider.cs
+++ src/Dynamic.Dns.Service/Repository/AddressProvider.cs
@@ -1,50 +1,55 @@
 ﻿using System;
 using System.Configuration;
+using System.Linq;
 using System.Threading.Tasks;
-using Dynamic.Dns.Contracts.Services;
+using Dynamic.Dns.Contracts.Repository;
 using Dynamic.Dns.Service.TableEntities;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
 
-namespace Dynamic.Dns.Service.Services
+namespace Dynamic.Dns.Service.Repository
 {
-    public class AddressWriter : IAddressWriter
+    public class AddressProvider : IAddressProvider
     {
         private readonly string _storageConnectionString;
 
-        public AddressWriter()
+        public AddressProvider()
         {
             _storageConnectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
         }
 
-        public async Task<bool> StoreIpAddress(string ipAddress)
+        public async Task<string> GetLatestAddress()
         {
             try
             {
                 var storageAccount = CloudStorageAccount.Parse(_storageConnectionString);
                 var tableClient = storageAccount.CreateCloudTableClient();
-
                 var table = tableClient.GetTableReference("IpAddresses");
-                await table.CreateIfNotExistsAsync();
 
-                var data = new IpAddressTable(ipAddress);
 
-                // Create the TableOperation object that inserts the customer entity.
-                var insertOperation = TableOperation.Insert(data);
+                var topItemQuery = new TableQuery<IpAddressTable>()
+                    .Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "ipaddress"))
+                    .Take(1);
 
-                // Execute the insert operation.
-                await table.ExecuteAsync(insertOperation);
-                return true;
+                var tableQuerySegment = await table.ExecuteQuerySegmentedAsync(topItemQuery, new TableContinuationToken());
+
+                if (tableQuerySegment.Results.Any())
+                {
+                    return tableQuerySegment.First()
+                        .Ipv4Address;
+                }
+
+                return string.Empty;
             }
             catch (StorageException)
             {
                 //TODO: Write Exceptions to Error Logger
-                return false;
+                return string.Empty;
             }
             catch (Exception)
             {
                 //TODO: Write Exceptions to Error Logger
-                return false;
+                return string.Empty;
             }
         }
     }
---
+++ src/Dynamic.Dns.Service/Repository/AddressWriter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using Dynamic.Dns.Contracts.Repository;
+using Dynamic.Dns.Service.TableEntities;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Dynamic.Dns.Service.Repository
+{
+    public class AddressWriter : IAddressWriter
+    {
+        private readonly string _storageConnectionString;
+
+        public AddressWriter()
+        {
+            _storageConnectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
+        }
+
+        public async Task<bool> StoreIpAddress(string ipAddress)
+        {
+            try
+            {
+                var storageAccount = CloudStorageAccount.Parse(_storageConnectionString);
+                var tableClient = storageAccount.CreateCloudTableClient();
+
+                var table = tableClient.GetTableReference("IpAddresses");
+                await table.CreateIfNotExistsAsync();
+
+                var data = new IpAddressTable(ipAddress);
+
+                // Create the TableOperation object that inserts the customer entity.
+                var insertOperation = TableOperation.Insert(data);
+
+                // Execute the insert operation.
+                await table.ExecuteAsync(insertOperation);
+                return true;
+            }
+            catch (StorageException)
+            {
+                //TODO: Write Exceptions to Error Logger
+                return false;
+            }
+            catch (Exception)
+            {
+                //TODO: Write Exceptions to Error Logger
+                return false;
+            }
+        }
+    }
+}
