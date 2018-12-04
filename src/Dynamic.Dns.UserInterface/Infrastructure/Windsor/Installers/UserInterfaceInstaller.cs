1381f92f182c24bf15d6a8b87dfab5925b9fdbc0
Aran Mulholland <aranmulholland@gmail.com>
2018-12-04 11:26:51 +1100

    Add installer for the user interface components

---
+++ src/Dynamic.Dns.UserInterface/Infrastructure/Windsor/Installers/UserInterfaceInstaller.cs
@@ -0,0 +1,19 @@
+﻿using Castle.Facilities.TypedFactory;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Dynamic.Dns.UserInterface.Infrastructure.Windsor.Factories;
+
+namespace Dynamic.Dns.UserInterface.Infrastructure.Windsor.Installers
+{
+    public class UserInterfaceInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.AddFacility<TypedFactoryFacility>();
+            container.Register(Component.For<IMainWindowFactory>().AsFactory());
+            container.Register(Component.For<MainWindow>().LifestyleTransient());
+            container.Register(Component.For<DynamicDnsUserInterfaceViewModel>().LifestyleTransient());
+        }
+    }
+}
