145fede21e52f11d28d641054cc1fbb526aaea45
Aran Mulholland <aranmulholland@gmail.com>
2018-12-04 13:50:06 +1100

    Update assembly info

--- src/Dynamic.Dns.Web/Properties/AssemblyInfo.cs
+++ src/Dynamic.Dns.Web/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Dynamic.Dns.Web")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Web site that returns the IP address of the calling computer and displays this and the RDP file donload")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Aran Mulholland")]
 [assembly: AssemblyProduct("Dynamic.Dns.Web")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
