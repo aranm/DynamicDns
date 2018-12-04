861bd97e59349dc52c77caf61ed8c79c740c2320
Aran Mulholland <aranmulholland@gmail.com>
2018-12-04 13:36:35 +1100

    Add download link for RDP file

--- src/Dynamic.Dns.Web/Controllers/HomeController.cs
+++ src/Dynamic.Dns.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.IO;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 using Dynamic.Dns.Contracts.Repository;
 using Dynamic.Dns.Web.Models;
@@ -26,6 +27,26 @@ namespace Dynamic.Dns.Web.Controllers
             };
 
             return View(ipAddressModel);
+        }
+
+        [Route("remote-desktop")]
+        public async Task<ActionResult> DownLoadRdpFile()
+        {
+            var ipAddress = await _addressProvider.GetLatestAddress();
+            ipAddress = ipAddress.Replace("\"", string.Empty);
+
+            using (var memoryStream = new MemoryStream())
+            {
+                TextWriter tw = new StreamWriter(memoryStream);
+
+                tw.WriteLine($"full address:s:{ipAddress}");
+                tw.WriteLine("prompt for credentials:i:1");
+                tw.WriteLine("administrative session:i:1");
+                tw.Flush();
+                tw.Close();
+
+                return File(memoryStream.GetBuffer(), "application/x-rdp", "myconnection.rdp");
+            }
         }
     }
 }
