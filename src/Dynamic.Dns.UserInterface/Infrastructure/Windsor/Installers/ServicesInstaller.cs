e60d7f79818ef6237a136bff24b2e42bf741b358
Aran Mulholland <aranmulholland@gmail.com>
2018-12-04 11:27:21 +1100

    Update service installer to remove unused components and register Address Service

--- src/Dynamic.Dns.UserInterface/Infrastructure/Windsor/Installers/ServicesInstaller.cs
+++ src/Dynamic.Dns.UserInterface/Infrastructure/Windsor/Installers/ServicesInstaller.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Castle.MicroKernel.Registration;
+﻿using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
+using Dynamic.Dns.Contracts.Repository;
 using Dynamic.Dns.Contracts.Services;
+using Dynamic.Dns.Service.Repository;
 using Dynamic.Dns.Service.Services;
-using Dynamic.Dns.UserInterface.Infrastructure.Windsor.Injection;
 
 namespace Dynamic.Dns.UserInterface.Infrastructure.Windsor.Installers
 {
@@ -16,13 +12,16 @@ namespace Dynamic.Dns.UserInterface.Infrastructure.Windsor.Installers
     {
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
-            container.Kernel.ComponentModelBuilder.AddContributor(new InjectionPropertyComponentModelHelper());
+            container.Register(Component.For<IAddressService>()
+                .ImplementedBy<AddressService>()
+                .LifestyleSingleton());
+
+            container.Register(Component.For<IAddressWriter>()
+                .ImplementedBy<AddressWriter>()
+                .LifestyleTransient());
 
             container.Register(Component.For<IAddressProvider>()
                 .ImplementedBy<AddressProvider>()
-                .LifestyleTransient());
-            container.Register(Component.For<IAddressWriter>()
-                .ImplementedBy<AddressWriter>()
                 .LifestyleTransient());
         }
     }
